78987411253b487c01d55e9a77d67e951c3b7a9e
Danny Cho <dannycho7@gmail.com>
2016-12-16 13:52:05 -0800

    Update settings for origin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,11 +56,11 @@ config.action_mailer.preview_path = "#{Rails.root}/app/mailer_previews"
 config.action_mailer.delivery_method = :smtp
 
 config.action_mailer.smtp_settings = {
-   address:              'smtp.sendgrid.net',
+   address:              'smtp.gmail.com',
    port:                 587,
-   domain:               'heroku.com',
-   user_name:             ENV['SENDGRID_USERNAME'], 
-   password:              ENV['SENDGRID_PASSWORD'],
+   domain:               'gmail.com',
+   user_name:             ENV['GMAIL_USERNAME'], 
+   password:              ENV['GMAIL_PASSWORD'],
    authentication:       'plain',
    openssl_verify_mode_: 'none',
    enable_starttls_auto: true  
